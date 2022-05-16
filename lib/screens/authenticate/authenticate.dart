4ff9e0aae0c44dba30513a8af183b6d7b60fcacd
utsav-07 <utsavudit07@gmail.com>
2022-05-16 23:55:50 +0530

    all code

---
+++ lib/screens/authenticate/authenticate.dart
@@ -0,0 +1,29 @@
+import 'package:bus_on_the_way/screens/authenticate/register.dart';
+import 'package:bus_on_the_way/screens/authenticate/sign_in.dart';
+import 'package:flutter/material.dart';
+ 
+class Authenticate extends StatefulWidget {
+  final String ? text;
+
+  Authenticate({this.text});
+  @override
+  _AuthenticateState createState() => _AuthenticateState();
+}
+
+class _AuthenticateState extends State<Authenticate> {
+
+  bool showSignIn = true;
+  void toogleView() {
+    setState(() => showSignIn = !showSignIn);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if(showSignIn) {
+      return SignIn(toogleView: toogleView , text : widget.text);
+    }
+    else {
+      return Register(toogleView: toogleView);
+    }
+  }
+}
